82a0b56f47c965eea8b96a285e4eebe4642f7253
mudroljub <mudroljub@gmail.com>
2018-10-11 20:34:13 +0200

    stabilno stanje

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import Footer from './Footer';
-import Header from './Header';
+import Footer from './components/Footer';
+import Movies from './components/Movies';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Movies/>
         <Footer/>
       </div>
     );
---
+++ src/components/Movies.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+// import filmovi from '../data/filmovi.json'
+import './Movies.css';
+
+class Movies extends Component {
+  state = {
+    filmovi: [],
+    isLoaded: false,
+  }
+
+  componentDidMount() {
+    fetch('https://baza-podataka.herokuapp.com/filmovi/')
+    .then(response => response.json())
+    .then(json => this.setState({
+      filmovi: json,
+      isLoaded: true
+    }))
+  }
+
+  render() {
+    const filmoviJsx = this.state.filmovi.map(film => (
+      <div key={film._id}>
+        <h3>{film.naziv}</h3>
+        <img src={film.slika} alt={film.naziv} />
+        <p>{film.godina}</p>
+      </div>
+    ))
+
+    return (
+      <div className="movies-wrapper">
+        {filmoviJsx}
+      </div>
+    )
+  }
+}
+
+export default Movies
