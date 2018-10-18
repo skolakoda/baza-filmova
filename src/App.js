41ad1debaaafd187539f8245d1e6bbe8e2588f19
mudroljub <mudroljub@gmail.com>
2018-10-18 20:21:41 +0200

    dodat ruter

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Footer from './components/Footer';
-import Movies from './components/Movies';
+import Main from './components/Main';
 import Header from './components/Header';
-import AddMovie from './components/AddMovie';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
-        <AddMovie/>
-        <Movies/>
+        <Main />
         <Footer/>
       </div>
     );
   }
 }
-
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+import Movies from './Movies';
+import AddMovie from './AddMovie';
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Movies}/>
+      <Route path='/add' component={AddMovie}/>
+    </Switch>
+  </main>
+)
+
+export default Main
