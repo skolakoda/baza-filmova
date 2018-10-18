0d320ebab6655850ac338ce3e150bff509c9a6fc
mudroljub <mudroljub@gmail.com>
2018-10-18 20:33:11 +0200

    dodata navigacija

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,10 @@ import React, {Component} from 'react'
 class Footer extends Component {
 
   render() {
-    const ime = "Damjan"
-    const pozdrav = "Zdravo korisniče "
     const datum = new Date().toLocaleDateString('sr')
 
     return (
       <footer>
-        <p>{pozdrav} {ime}</p>
         <p>Danas je {datum} </p>
       </footer>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
+
+const linkStyle = {
+  color: 'white'
+}
 
 class Header extends Component {
   render() {
     return (
       <header className="App-header">
+        <nav>
+          <Link style={linkStyle} to='/'>Home</Link>{' '}
+          <Link style={linkStyle} to='/add'>Add movie</Link>
+        </nav>
         <h1>Baza filmova</h1>
-        <h2>Zdravo {this.props.ime}</h2>
       </header>
     )
   }
