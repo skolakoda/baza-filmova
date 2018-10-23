bc81fc12dc488a2c6c751969cff432527b8a5c2b
kicaubuntu <kicaubuntu@gmail.com>
2018-10-23 21:00:21 +0200

    filtersAndsort

--- src/App.js
+++ src/App.js
@@ -6,12 +6,27 @@ import Main from './components/Main';
 import Header from './components/Header';
 
 class App extends Component {
+  state = {
+    counter:0,
+  }
+  counterMin = () => {
+    console.log("COUNTER",this.state.counter)
+   this.setState({
+     counter:this.state.counter-1
+   })
+  }
+  counterMax = () => {
+    console.log("COUNTER",this.state.counter)
+    this.setState({
+      counter:this.state.counter+1
+    })
+   }
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header counter={this.state.counter}/>
         <Main />
-        <Footer/>
+        <Footer umanjenje={this.counterMin} uvecanje={this.counterMax}/>
       </div>
     );
   }
