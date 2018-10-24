83e34b71c0ef7cee86cb5bdc7b905322ce0791b2
Milan <milanajtic@gmail.com>
2018-10-24 02:27:46 +0200

    prettier ponovo

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,18 +1,16 @@
-import React, {Component} from 'react'
-
+import React, { Component } from "react";
 
 class SingleMovie extends Component {
-  state = {
-  }
-  
+  state = {};
+
   render() {
-    console.log("PROPS",this.props.location.state)
+    console.log("PROPS", this.props.location.state);
     return (
-     <div>
-         <h1>Single Movie</h1>
-     </div>
-    )
+      <div>
+        <h1>Single Movie</h1>
+      </div>
+    );
   }
 }
 
-export default SingleMovie
+export default SingleMovie;
