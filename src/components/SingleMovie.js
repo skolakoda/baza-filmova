8d2ddfc1634123fa9123be67c516f8672f85eb0a
mudroljub <mudroljub@gmail.com>
2018-10-25 19:58:26 +0200

    komentari

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,15 +1,32 @@
 import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
 
+import Movie from './Movie'
 
 class SingleMovie extends Component {
   state = {
   }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(e.target.ime.value);
+    console.log(e.target.komentar.value);
+  }
   
   render() {
-    console.log("PROPS",this.props.location.state)
     return (
      <div>
-         <h1>Single Movie</h1>
+        <Link to='/'>&lt; Back</Link>{' '}
+        <Movie podaci={this.props.location.state} />
+
+        <form onSubmit={this.handleSubmit}>
+          <label>Ime:</label><br/>
+          <input name="ime" /><br/>
+          <label>Komentar: </label><br/>
+          <textarea name="komentar" /><br/>
+          <button>Submit</button>
+        </form>
+
      </div>
     )
   }
