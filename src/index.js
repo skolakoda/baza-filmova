72650bcd0b2c478d4193b0a35a28bc74bf725fdf
mudroljub <mudroljub@gmail.com>
2018-11-01 20:39:22 +0100

    fix routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 import './index.css';
 import App from './App';
 
+const basename = process.env.NODE_ENV === 'development' ? '' : 'programerski-citati'
+
 ReactDOM.render((
-  <BrowserRouter>
+  <Router basename={basename}>
     <App />
-  </BrowserRouter>
+  </Router>
 ), document.getElementById('root'))
