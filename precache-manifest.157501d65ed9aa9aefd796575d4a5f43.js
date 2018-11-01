3fc676421efb70995a2a569eeb578a3e6135d87d
mudroljub <mudroljub@gmail.com>
2018-11-01 20:15:58 +0100

    Updates

---
+++ precache-manifest.157501d65ed9aa9aefd796575d4a5f43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef3d0743be4e8c3555ad",
+    "url": "/baza-filmova/static/js/runtime~main.ef3d0743.js"
+  },
+  {
+    "revision": "4e15eae26201b2c42fd9",
+    "url": "/baza-filmova/static/js/main.4e15eae2.chunk.js"
+  },
+  {
+    "revision": "2afdfb144f7f48bf0683",
+    "url": "/baza-filmova/static/js/1.2afdfb14.chunk.js"
+  },
+  {
+    "revision": "4e15eae26201b2c42fd9",
+    "url": "/baza-filmova/static/css/main.f448eb88.chunk.css"
+  },
+  {
+    "revision": "1486b125ab3e72da73deb21ea03d83f5",
+    "url": "/baza-filmova/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/baza-filmova/precache-manifest.157501d65ed9aa9aefd796575d4a5f43.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/baza-filmova/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
