3130113e975eeba58170e0b3cb4df8bc670050bd
Milan <milanajtic@gmail.com>
2018-11-02 21:05:03 +0100

    hash router umesto browser routera

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom'
->>>>>>> e63d3eb5995607ec4389c2c03c60a6b6ed0c72fe
+import { HashRouter as Router } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 
-<<<<<<< HEAD
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
-=======
-ReactDOM.render((
   <Router>
     <App />
-  </Router>
-), document.getElementById('root'))
->>>>>>> e63d3eb5995607ec4389c2c03c60a6b6ed0c72fe
+  </Router>,
+  document.getElementById("root")
+);
