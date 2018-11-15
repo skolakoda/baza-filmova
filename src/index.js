1115517837de5655c1456567453838b56eb82de2
mudroljub <mudroljub@gmail.com>
2018-11-15 18:58:14 +0100

    install redux and create reducer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
+import {reducer} from './store'
 import "./index.css";
 import App from "./App";
 
+const store = createStore(reducer)
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
